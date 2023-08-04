293f30264ee86c203730ac7270315fdbfbf06f19
josess482 <josess482@gmail.com>
2023-08-04 17:32:06 +0200

    _Creo modelo para usuario

---
+++ src/main/java/com/ays/ms/model/User.java
@@ -0,0 +1,20 @@
+package com.ays.ms.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String name;
+    private String lastName;
+    private String secondLastName;
+    private String email;
+    private LocalDateTime dateBirth;
+
+}

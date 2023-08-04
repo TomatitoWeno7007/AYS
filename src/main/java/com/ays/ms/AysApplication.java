cbec08d7be66bb1676d46350a22a1beb4b5ffb7e
josess482 <josess482@gmail.com>
2023-08-04 17:05:31 +0200

    Se crea la estructura básica de la aplicación

---
+++ src/main/java/com/ays/ms/AysApplication.java
@@ -0,0 +1,13 @@
+package com.ays.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ays/ms/AysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ays.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
